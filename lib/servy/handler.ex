049d0019762d2f78f755a722a41cb3cbc1532951
Stephen McCullough <stephen.mccullough@gmail.com>
2019-08-17 22:01:38 +0100

    Added Request Params and Status Code

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -14,7 +14,10 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ") 
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method, 
+       path: path, 
+       resp_body: "",
+       status: nil }
   end
 
   def route(conv) do
@@ -22,16 +25,24 @@ defmodule Servy.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Smokey, Paddington, Yogi" }
+    %{ conv | status: 200, resp_body: "Smokey, Paddington, Yogi" }
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "Couldn't find #{path}!"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -40,6 +51,16 @@ defmodule Servy.Handler do
   end
 
   def log(conv), do: IO.inspect conv
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
 
 request = """
@@ -66,6 +87,17 @@ IO.puts response
 
 request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
