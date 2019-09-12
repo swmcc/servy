65f37371028359d479ebadd3647d73e05f180b8d
Stephen McCullough <stephen.mccullough@gmail.com>
2019-09-12 12:24:09 +0100

    Added comprehensions in the new templates. Added templates instad of rendering HTML inline. Added a BearView module to access templates.

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -2,27 +2,22 @@ defmodule Servy.BearController do
 
   alias Servy.Wildthings
   alias Servy.Bear
-
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>" 
-  end
+  alias Servy.BearView
 
   @doc "The index for bears"
   def index(conv) do
-    items = 
+    bears = 
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join
-
-    %{ conv | status: 200, resp_body: "<ul>#{items}</ul>" }
+    
+    %{ conv | status: 200, resp_body: BearView.index(bears)}  
   end
 
   @doc "Shows a specific bear"
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
-    %{ conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}" }
+
+    %{ conv | status: 200, resp_body: BearView.show(bear)}  
   end
 
   @doc "Shows the success page for creating a bear"
