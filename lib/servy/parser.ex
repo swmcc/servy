0259ba43c623053b0e7569b5c292aa20bc007f61
Stephen McCullough <stephen.mccullough@gmail.com>
2019-09-12 19:18:06 +0100

    Added tests. Removed hardcoded 'tests' from handler.ex

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -4,8 +4,8 @@ defmodule Servy.Parser do
 
   @doc "Parse the request to get the METHOD and the PATH of the request."
   def parse(request) do
-    [top, params_string] = String.split(request, "\n\n")
-    [request_line | header_lines] = String.split(top, "\n")
+    [top, params_string] = String.split(request, "\r\n\r\n")
+    [request_line | header_lines] = String.split(top, "\r\n")
     [method, path, _] = String.split(request_line, " ")
     headers = parse_headers(header_lines, %{})
     params = parse_params(headers["Content-Type"], params_string)
@@ -18,10 +18,21 @@ defmodule Servy.Parser do
     }
   end
 
+  @doc """
+  Parses the given param string of the form `key1=value1&key2=value2` into a map with corresponding keys and values.
+
+  ## Examples
+    iex> params_string = "name=Baloo&Type=Brown"
+    iex> Servy.Parser.parse_params("application/x-www-form-urlencoded", params_string)
+    %{"name" => "Baloo", "Type" => "Brown"}
+    iex> Servy.Parser.parse_params("multipart/form-data", params_string)
+    %{}
+  """
+  
   def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim |> URI.decode_query
   end
-
+  
   def parse_params(_, _), do: %{}
 
   def parse_headers([head | tail], headers) do
